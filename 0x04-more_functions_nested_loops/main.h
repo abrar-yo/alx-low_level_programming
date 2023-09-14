5376ec2bde25a52fc75a0cec2ea14ed6e28c2e60
abrar-yo <abraryousif6@gmail.com>
2023-09-14 14:23:14 -0700

    is upper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+int _putchar(char c);
+
+ int _isupper(int c);
+
+#endif /* _MAIN_H_ */
